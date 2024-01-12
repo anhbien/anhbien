0986764ac9a200f909652fdc5003f094fd24e5e0
anh bien <anhnbien@gmail.com>
2024-01-12 12:42:14 -0600

    Updates

---
+++ precache-manifest.ce8615b1497a02e87141acd3a2fd5a97.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8231b2e5cf377e270f40fd14fcd9b2fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972d24cb6cb13be5809e",
+    "url": "/static/css/main.0d2ed816.chunk.css"
+  },
+  {
+    "revision": "930303b918817d78abde",
+    "url": "/static/js/2.6b32572f.chunk.js"
+  },
+  {
+    "revision": "a68fb9a705c058f3aaed8f74c7821cda",
+    "url": "/static/js/2.6b32572f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972d24cb6cb13be5809e",
+    "url": "/static/js/main.8bc8dd76.chunk.js"
+  },
+  {
+    "revision": "f39648fb9d09f84fad84",
+    "url": "/static/js/runtime-main.e6243b3c.js"
+  },
+  {
+    "revision": "fd94d92f24ad63e54e49c5b64ed33f5b",
+    "url": "/static/media/Logo.fd94d92f.svg"
+  },
+  {
+    "revision": "74afb8dcfcc1a0de0697a7af31b7b87b",
+    "url": "/static/media/LogoReverse.74afb8dc.svg"
+  },
+  {
+    "revision": "d1ab8a0db59cdf7b5a1086ec32dded70",
+    "url": "/static/media/Thumbnail.d1ab8a0d.jpg"
+  },
+  {
+    "revision": "8050889cfc07d46e018f1e14045fd2c3",
+    "url": "/static/media/code.8050889c.svg"
+  },
+  {
+    "revision": "2d40e358e156601f089c325814a3d2aa",
+    "url": "/static/media/design.2d40e358.svg"
+  },
+  {
+    "revision": "3016ae2a27f037890f057d04fcd021c0",
+    "url": "/static/media/location.3016ae2a.svg"
+  },
+  {
+    "revision": "28d37ff823eb49eac96b61c2e55cb609",
+    "url": "/static/media/me.28d37ff8.svg"
+  }
+]);
